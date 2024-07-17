bdd7c265f31e4e7c8fe734ddfaae445a20e2539d
Anup <anupthedev@gmail.com>
2024-07-17 15:44:19 -0400

    Adjusted sizes

--- src/app/ui/components/Header.tsx
+++ src/app/ui/components/Header.tsx
@@ -87,7 +87,7 @@ export function Header() {
 			{/* Search Input */}
 			<div className='flex justify-center relative'>
 				<form
-					className={`flex items-center justify-between  bg-white md:bg-opacity-20 text-md pl-3 border-2 border-black/30 rounded-xl relative md:static ${
+					className={`flex items-center justify-between bg-white md:bg-opacity-20 text-md pl-3 border-2 border-black/30 rounded-xl relative md:static ${
 						workSans.className
 					} z-20 ${isSearchVisible ? 'flex' : 'hidden md:flex'} ${
 						isInputFocused ? 'shadow-lg' : ''
