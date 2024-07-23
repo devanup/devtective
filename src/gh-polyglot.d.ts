523cd1519bc3802a3e08997f45fc9038a40e1a55
Anup <anupthedev@gmail.com>
2024-07-23 18:58:04 -0400

    feat: replace Commit Activity with Top Contributing Repos UI

---
+++ src/gh-polyglot.d.ts
@@ -0,0 +1,11 @@
+// src/gh-polyglot.d.ts
+
+declare module 'gh-polyglot' {
+	export default class GhPolyglot {
+		constructor(username: string);
+
+		repoStats(callback: (err: any, stats: any) => void): void;
+		userStats(callback: (err: any, stats: any) => void): void;
+		getAllRepos(callback: (err: any, stats: any) => void): void;
+	}
+}
